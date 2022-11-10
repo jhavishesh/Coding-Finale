3bb3b4a67e8fb8a9600e56c9eaac9d972892526b
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-11-11 01:23:17 +0530

    commit

--- Question 2/Java_Code.java
+++ Question 2/Java_Code.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-class Test {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader read =
             new BufferedReader(new InputStreamReader(System.in));
